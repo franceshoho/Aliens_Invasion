a6f2545a0fbedb61786b7fe8419aba49c2cb5621
franceshoho <francesho.kids@gmail.com>
2020-10-02 10:49:01 -0700

    Added initial codes up to ship image and ship class

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """Class to manage game assets and behaviors"""
@@ -8,12 +9,17 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game and create game resources"""
         pygame.init()
+        # set screem
         self.settings = Settings()  # init Settings for screen
+        # set screen size
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
-        )  # set screen size
         pygame.display.set_caption("Alien Invasion")
         self.bg_color = (self.settings.bg_color) # set background color
+
+        # initiate ship class
+        self.ship = Ship(self)
+
 
     def run_game(self):
         """Start main loop of the game"""
@@ -24,6 +30,7 @@ class AlienInvasion:
                     sys.exit()
 
             self.screen.fill(self.bg_color)
+            self.ship.blitme()
             # Fresh screen.  Make the most recently drawn screen visible
             pygame.display.flip()
 
