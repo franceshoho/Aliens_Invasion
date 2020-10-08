199a0626d6723f927fa3f2581e5d43767f9404d4
franceshoho <francesho.kids@gmail.com>
2020-10-08 16:45:53 -0700

    repopulate fleet if destroyed and empty bullets

--- settings.py
+++ settings.py
@@ -12,12 +12,12 @@ class Settings:
         self.bg_color = (230, 230, 230)
 
         # ship settings
-        self.ship_speed = 1.5 
+        self.ship_speed = 3
 
         # bullet settings - dark grey 3x15 pixels
-        self.bullet_speed = 3
+        self.bullet_speed = 7
         self.bullet_width = 3
-        self.bullet_height = 15
+        self.bullet_height = 10
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 5
 
